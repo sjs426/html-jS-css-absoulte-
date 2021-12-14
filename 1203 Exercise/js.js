097cd65300343b4a4bc07bee839d5baacabf3f31
sjs426 <77316050+sjs426@users.noreply.github.com>
2021-12-14 21:45:26 +0800

    Add files via upload

---
+++ 1203 Exercise/js.js
@@ -0,0 +1,31 @@
+
+    if(screen.width<800 || screen.height<600)
+    {alert("Best resoultion: \n 800*600")}
+
+    <!-- <script> 
+                var name;
+        name = widow.prompt("Please enter your name");
+        document.writeln("<h1>Hello " + name + ", Welcome to JavaScript programming!</h1>");
+    </script>
+
+    <script>
+        if(screen.width<800 || screen.height<600)
+            {alert("Best resoultion: \n 800*600")}
+    
+        </script>
+    
+    <script>
+widnow.prompt();
+let grade = 66
+    if (grade >= 60) {
+        document.write("Pass!");
+    } else {
+        document.write("c u next year");
+    }
+</script>-->
+
+
+            if (number == rand) {
+                alert("Do it again");
+                number = window.prompt("Put a number from 1~10");
+            }
